71b3c733e56b57ddc6f9050991c2bc628a609263
GiliardGodoi <giliardgodoi@alunos.utfpr.edu.br>
2018-04-16 14:07:53 -0300

    Castalia-K/Simulations/CL/comp.sh

--- Castalia-K/Simulations/CL/comp.sh
+++ Castalia-K/Simulations/CL/comp.sh
@@ -3,5 +3,5 @@
 rm *.txt
 
 #Castalia -c ZigBeeMAC,General,setRate,varyInterferenceModel,varyJanela -r 13 -o saida.txt
-Castalia -c ZigBeeMAC,General,setRate,setInterferenceModel -r 13 -o saida.txt
+Castalia -c ZigBeeMAC,General,setRate,setInterferenceModel -r 33 -o saida.txt
 

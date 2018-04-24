830cd6a419e842578ee4f5e791ce0b0cc06e1c4c
GiliardGodoi <giliardgodoi@alunos.utfpr.edu.br>
2018-04-24 10:17:20 -0300

    Extraindo dados do CastaliaResults: packets, energy e breakdown

--- Castalia-K/Simulations/CL/comp.sh
+++ Castalia-K/Simulations/CL/comp.sh
@@ -2,10 +2,17 @@
 
 rm *.txt
 
-#Castalia -c ZigBeeMAC,General,setRate,varyInterferenceModel,varyJanela -r 13 -o saida.txt
-Castalia -c ZigBeeMAC,General,setRate,varyInterferenceModel -r 33 -o saida.txt
-#Castalia -c ZigBeeMAC,General,setRate,setInterferenceModel -r 33 -o saida.txt
+#Castalia -c ZigBeeMAC,General,setRate,varyInterferenceModel -r 33 -o saida.txt
+Castalia -c ZigBeeMAC,General,setRate,setInterferenceModel -r 33 -o saida.txt
 
 CastaliaResults -i saida.txt -s packets -n -o2 > packets.txt
 CastaliaResults -i saida.txt -s energy -n -o2 > energy.txt
 CastaliaResults -i saida.txt -s breakdown -n -o2 > breakdown.txt
+
+destino="/home/giliard/Documentos/resultado-simulacoes/resultado"
+
+mv *.txt $destino
+
+rm *.txt
+
+Castalia -c ZigBeeMAC,General,setRate,varyInterferenceModel -r 33 -o saida.txt

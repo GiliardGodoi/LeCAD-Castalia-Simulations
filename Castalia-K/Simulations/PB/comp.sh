e7715f2b4ec871a2ab08c68119a45f9a65139f17
GiliardGodoi <giliardgodoi@alunos.utfpr.edu.br>
2018-05-24 19:01:54 -0300

    SIMUALÇÕES PA COM OS MESMOS PARÂMETROS DE PB
    Arquivos para obter os dados de taxa MAC tanto da camada de aplicação
    quanto da camada MAC
    
    comp.sh automatizando geração dos arquivos de dados.
    Seguir o mesmo padrã para nomeação dos arquivos de saída.

--- Castalia-K/Simulations/PB/comp.sh
+++ Castalia-K/Simulations/PB/comp.sh
@@ -1,8 +1,20 @@
 #!/bin/sh
 
 rm *.txt
+rm *.csv
 
-Castalia -c General -r 3 -o saida.txt
+#Castalia -c General -r 3 -o saida.txt
 #Castalia -c BaselineMAC,General,setRate,setPower,varyInterferenceModel,setPotencia1 -r 3 -o saida.txt
 # CastaliaResults -i saida.txt -s packet -n
 # CastaliaResults -i saida.txt -s received -n -o 2 > temp.txt
+
+echo "Executando simulação\n"
+
+Castalia -c General,ZigBeeMAC,setRate,setInterferenceModel,traceFromApp,traceFromMac -r 100 -o saida.txt
+
+echo "EXECUTANDO awk\n"
+
+awk -f janelaMAC.awk Castalia-Trace.txt > dataTaxaMacFromMacLayer.csv
+awk -f taxamac.awk Castalia-Trace.txt > dataTaxaMacFromAppLayer.csv
+
+echo "FIM DA SIMULAÇÃO PB"

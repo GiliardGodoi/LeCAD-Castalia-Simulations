adcefe553a781725362b35c5713077d71784bd1f
Giliard Godoi <giliardgodoi@alunos.utfpr.edu.br>
2019-05-28 22:31:06 -0300

    Zerando diretorio simulacoes.
     Mudanças a serem submetidas:
    	deleted:    Castalia-K/Simulations/taxaMAC-Potencia/*

--- Castalia-K/Simulations/taxaMAC-Potencia/comp.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-rm *.txt
-
-
-Castalia -c ZigBeeMAC,General,setRate,setInterferenceModel,setJanela,setPower -r 1 -o saida.txt
-
--- Castalia-K/Simulations/taxaMAC-Potencia/comp2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-rm *.txt
-
-Castalia -c ZigBeeMAC,General,setRate,setInterferenceModel,setPower,setJanela -r 1 -o saida.txt
-
-# para usar o arquivo mostra-taxaMAC.awk
-# awk -f mostra-taxaMAC.awk Castalia-Trace.txt 

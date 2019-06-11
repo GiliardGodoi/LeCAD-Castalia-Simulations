59d0c6fb7024ca9c61b373a993792c7d0d34ab95
Giliard Godoi <giliardgodoi@alunos.utfpr.edu.br>
2019-06-10 23:33:54 -0300

    Configuração das Simulações

--- Castalia-K/Simulations/executar.sh
+++ Castalia-K/Simulations/executar.sh
@@ -19,51 +19,68 @@ listar_dados()
     CastaliaResults -i saida.txt -s "Remaining Energy" -n -p -o2 > energy_remaining.txt
 }
 
+listar_dados_all()
+{
+    CastaliaResults -i saida.txt -s "Packet Breakdown" -n -p -o2 --all > packet_breakdown_all.txt
+    CastaliaResults -i saida.txt -s "Packets reception rate" -n -p -o2 --all > packets_reception_rate_all.txt
+    CastaliaResults -i saida.txt -s "Packets received per node" -n -p -o2 --all > packets_received_per_node_all.txt
+    CastaliaResults -i saida.txt -s "Packets loss rate" -n -p -o2 --all > packets_loss_rate_all.txt
+
+    CastaliaResults -i saida.txt -s "Estimated network lifetime" -p -o2 > estimated_network_lifetime.txt
+    CastaliaResults -i saida.txt -s "Energy nJ/bit" -n -p -o2 --all > energy_NJ_per_bit_all.txt
+    CastaliaResults -i saida.txt -s "Consumed Energy" -n -p -o2 --all > energy_consumed_all.txt
+    CastaliaResults -i saida.txt -s "Remaining Energy" -n -p -o2 --all > energy_remaining_all.txt
+}
+
 repeticao=3
 
 # for estrategia in CLBinomial CL2 CL4 PA PB
 # do
-#     for taxa in setRateLow setRateMedium setRateHigh
+#     for taxa[setRateLow,setRateMedium,setRateHigh] in setRateLow setRateMedium
 #     do
 #         diretorio=$estrategia$taxa
 #         echo  "Executando simulação $diretorio "
 #     done
 # done
 
-# echo "SIMULAÇÃO CL Binomial"
-# mkdir CLBinomial
-# cp omnetpp.ini  CLBinomial/
-# cd CLBinomial/
-# Castalia -c General,CLBinomial,setRateHigh,varyInterferenceModel,collectTrace -r $repeticao  -o saida.txt
-# listar_dados
-# cd ..
-# echo "...fim simulação"
+echo "SIMULAÇÃO CL Binomial"
+mkdir CLBinomial
+cp omnetpp.ini  CLBinomial/
+cd CLBinomial/
+Castalia -c General,CLBinomial,varyInterferenceModel,collectTrace[setRateLow,setRateMedium,setRateHigh] -r $repeticao  -o saida.txt
+listar_dados
+listar_dados_all
+cd ..
+echo "...fim simulação"
 
 
-# echo "SIMULAÇÃO CL CL4"
-# mkdir CL4
-# cp omnetpp.ini  CL4/
-# cd CL4/
-# Castalia -c General,CL4,setRateHigh,varyInterferenceModel,collectTrace -r $repeticao  -o saida.txt
-# listar_dados
-# cd ..
-# echo "...fim simulação"
+echo "SIMULAÇÃO CL CL4"
+mkdir CL4
+cp omnetpp.ini  CL4/
+cd CL4/
+Castalia -c General,CL4,varyInterferenceModel,collectTrace[setRateLow,setRateMedium,setRateHigh] -r $repeticao  -o saida.txt
+listar_dados
+listar_dados_all
+cd ..
+echo "...fim simulação"
 
-# echo "SIMULAÇÃO CL CL2"
-# mkdir CL2
-# cp omnetpp.ini  CL2/
-# cd CL2/
-# Castalia -c General,CL2,setRateHigh,varyInterferenceModel,collectTrace -r $repeticao  -o saida.txt
-# listar_dados
-# cd ..
+echo "SIMULAÇÃO CL CL2"
+mkdir CL2
+cp omnetpp.ini  CL2/
+cd CL2/
+Castalia -c General,CL2,varyInterferenceModel,collectTrace[setRateLow,setRateMedium,setRateHigh] -r $repeticao  -o saida.txt
+listar_dados
+listar_dados_all
+cd ..
 # echo "...fim simulação"
 
 echo "SIMULAÇÃO PA"
 mkdir PA
 cp omnetpp.ini  PA/
 cd PA/
-Castalia -c General,PA,setRateHigh,varyInterferenceModel,collectTrace -r $repeticao  -o saida.txt --debug
+Castalia -c General,PA,varyInterferenceModel,collectTrace[setRateLow,setRateMedium,setRateHigh] -r $repeticao  -o saida.txt
 listar_dados
+listar_dados_all
 cd ..
 echo "...fim simulação"
 
@@ -71,7 +88,8 @@ echo "SIMULAÇÃO PB"
 mkdir PB
 cp omnetpp.ini  PB/
 cd PB/
-Castalia -c General,PB,setRateHigh,varyInterferenceModel,collectTrace -r $repeticao  -o saida.txt --debug
+Castalia -c General,PB,varyInterferenceModel,collectTrace[setRateLow,setRateMedium,setRateHigh] -r $repeticao  -o saida.txt
 listar_dados
+listar_dados_all
 cd ..
 echo "...fim simulação"
